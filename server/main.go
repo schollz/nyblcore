a24967d48900bc5b999bcb13ff862fb34a2640cb
Zack <zack.scholl@gmail.com>
2023-04-03 09:33:29 -0700

    add basic server for creating patches

---
+++ server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("/", "./template")
+	router.POST("/", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Multipart form
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+		files := form.File["files"]
+
+		for _, file := range files {
+			filename := filepath.Base(file.Filename)
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+				return
+			}
+		}
+
+		fmt.Println(name, email)
+
+		converted := `#include <something>
+function main() {
+
+}
+	`
+		c.String(http.StatusOK, converted)
+	})
+	router.Run(":8080")
+}
